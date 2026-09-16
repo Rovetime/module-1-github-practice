301142e3f04c54b2235efa0ee2bd8b3f851483b5
Rovetime <itriago.s239866@stu.sanjac.edu>
2026-09-16 01:08:48 +0000

    Add AsyncStorage cart service

--- src/services/cartStorage.js
+++ src/services/cartStorage.js
@@ -3,18 +3,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const CART_STORAGE_KEY = 'northstar-shopping-cart';
 
 export async function saveCart(items) {
-  // TODO 1:
-  // Convert the cart array to JSON text and save it with AsyncStorage.setItem().
+  await AsyncStorage.setItem(CART_STORAGE_KEY, JSON.stringify(items));
 }
 
 export async function loadCart() {
-  // TODO 2:
-  // Read CART_STORAGE_KEY with AsyncStorage.getItem().
-  // If nothing has been saved, return [].
-  // If data exists, convert it back to JavaScript with JSON.parse().
+  const storedCart = await AsyncStorage.getItem(CART_STORAGE_KEY);
+
+  if (!storedCart) {
+    return [];
+  }
+
+  return JSON.parse(storedCart);
 }
 
 export async function clearSavedCart() {
-  // TODO 3:
-  // Remove only CART_STORAGE_KEY. Do not use AsyncStorage.clear().
+  await AsyncStorage.removeItem(CART_STORAGE_KEY);
 }
